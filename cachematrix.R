f11fc312cfe94587711387480586aadeeb534939
astuka <jrobins855@gmail.com>
2025-04-21 11:30:51 -0700

    Solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix() stores a computed inverted matrix to short-term memory. cacheSolve() 
+## will solve for an inverted matrix, while first seeing if such a matrix is already in memory.
 
-## Write a short comment describing this function
+##  Logic for caching an inverted matrix in memory via the "inv" variable.
 
 makeCacheMatrix <- function(x = matrix()) {
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) inv <<- inverse 
+        getinverse <- function() inv
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## Solves for an inverted matrix, then sends it to the cache. However, if the matrix had
+## already been inverted, it simply grabs it from the stored "inv" variable.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinverse()
+        if(!is.null(inv)) {
+                message("Getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setinverse(inv)
+        inv
 }
